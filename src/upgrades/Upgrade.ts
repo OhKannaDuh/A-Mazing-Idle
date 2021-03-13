1401fc95e0786c4270090f800a762eee9794a536
ImGregHenry <imgreghenry@gmail.com>
2021-03-13 14:55:25 -0800

    Folder restructure and small refactors.

--- src/upgrades/Upgrade.ts
+++ src/upgrades/Upgrade.ts
@@ -25,7 +25,7 @@ class Upgrade {
     this.initClickEvent();
   }
 
-  buyUpgrade = () => {
+  public buyUpgrade = () => {
     if (!this.canAffordToBuyUpgrade()) {
       console.error('Cannot afford to buy.');
       return;
@@ -37,58 +37,58 @@ class Upgrade {
     this.updateVisibility();
   }
 
-  getIsUpgraded(): boolean {
+  public getIsUpgraded(): boolean {
     return this.upgradeLevel >= 1;
   }
 
-  canAffordToBuyUpgrade(): boolean {
+  public canAffordToBuyUpgrade(): boolean {
     return IS_FREE_MODE_ENABLED || this.getCost() <= this.game.points.points;
   }
   
-  setUiText(text: string): void {
+  public setUiText(text: string): void {
     $(`#${this.uiId}`).attr('title', this.tooptipText);
     $(`#${this.uiId}`).text(text);
   }
 
-  updateVisibility(): void {
+  public updateVisibility(): void {
     $(`#${this.uiId}`).css('display',  (this.isUnlocked() && !this.isMaxUpgradeLevel()) ? 'block' : 'none');
   }
 
-  updateUiDisabled(): void {
+  public updateUiDisabled(): void {
     $(`#${this.uiId}`).prop("disabled", this.isDisabled());
   }
 
-  initClickEvent(): void {
+  public initClickEvent(): void {
     $(`#${this.uiId}`).unbind("click");
     $(`#${this.uiId}`).click(() => this.buyUpgrade());
   }
 
-  isDisabled(): boolean {
+  public isDisabled(): boolean {
     //TODO: refactor out isSinglePurchase
-    return (this.isSinglePurchase && this.upgradeLevel >= 1) || this.isMaxUpgradeLevel() || !this.canAffordToBuyUpgrade();
+    return this.isMaxUpgradeLevel() || !this.canAffordToBuyUpgrade();
   }
   
-  updateUiProperties(): void {
+  public updateUiProperties(): void {
     throw 'updateUiProperties must be implemented.';
   }
 
-  getCost(): number {
+  public getCost(): number {
     throw 'getCost must be implemented.';
   }
 
-  isUnlocked(): boolean {
+  public isUnlocked(): boolean {
     return this.game.biomes.isUpgradeUnlocked(this.upgradeKey);
   }
 
-  prettyPrint(val): string {
+  public prettyPrint(val): string {
     return UserInterface.getPrettyPrintNumberNoDecimals(val);
   }
 
-  getPrettyPrintCost(): string {
+  public getPrettyPrintCost(): string {
     return this.prettyPrint(this.getCost());
   }
 
-  isMaxUpgradeLevel(): boolean {
+  public isMaxUpgradeLevel(): boolean {
     return this.isSinglePurchase && this.upgradeLevel >= 1;
   }
 }
